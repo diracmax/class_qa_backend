101a01a062922510395d6c65fb157d4a83f26ef3
diracmax <you@example.com>
2022-05-17 20:16:41 +0900

    エンコード関連

--- api/library/certification.py
+++ api/library/certification.py
@@ -1,4 +1,7 @@
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "a1c3b64ba5258388022a17305b5684c6ca5c2eea79623bf3475cf5a5fee498ba"
-ALGORITHM = "HS256"
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
